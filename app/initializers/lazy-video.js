4928b5c224a5cae851c341cbd88c93f860f99b0a
Mykola Basov <kolybasov@yandex.ru>
2015-11-15 13:22:09 +0200

    Fix deprecations

--- app/initializers/lazy-video.js
+++ app/initializers/lazy-video.js
@@ -2,7 +2,7 @@ import LazyVideoProviders from 'ember-lazy-video/services/lazy-video-providers';
 
 export default {
   name: 'flash-messages',
-  initialize: function(container, application){
+  initialize: function(application){
     application.register('service:lazy-video-providers', LazyVideoProviders, { singleton: true });
     application.inject('component:lazy-video', 'providers', 'service:lazy-video-providers');
   }
